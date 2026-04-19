adb5b69a4734c9fd8678a25ef0408a8b9aa8db42
Ravinder <ravinderkumar@machwan.in>
2026-04-19 22:56:52 +0530

    added Controller and views

---
+++ src/Project/IntentMyDataKart/Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntentMyDataKart.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult CompanyRegistration()
+        {
+            return View();
+        }
+    }
+}
