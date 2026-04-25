29b3ea4f9831159376010a4b1065d4e084ab6ac4
Ravinder <ravinderkumar@machwan.in>
2026-04-25 12:50:53 +0530

    Added UserController

---
+++ src/Project/IntentMyDataKart/Database/DAL/DataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace IntentMyDataKart.Database.DAL
+{
+    public static class DataAccess
+    {
+        public static IDataAccess GetDataAccess()
+        {
+            return new SqlDataAccess();
+        }
+    }
+}
---
+++ src/Project/IntentMyDataKart/Models/UserRegistrationModel.cs
@@ -0,0 +1,20 @@
+﻿namespace IntentMyDataKart.Models
+{
+    public class UserRegistrationModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public long? CompanyRegistrationId { get; set; }
+        public int? RoleId { get; set; }
+        public string? State { get; set; }
+        public string? City { get; set; }
+        public string? Pincode { get; set; }
+        public string? Address { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+    }
+}
